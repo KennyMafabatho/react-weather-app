904e8123d406b12cd658c6b6d56cc9ca276414e6
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2022-07-19 11:45:20 +0200

    Created api components and added style on App.css

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5b2c794d8fmsh9681f58cc6a95dap11fca7jsn4424dd69c535",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
