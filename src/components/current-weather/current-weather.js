7beb2dbbbe508be8f844f911d09af61d742a69ea
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2022-08-04 10:40:59 +0200

    Added style to weather component

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,15 +1,41 @@
-import "./current-weather.css"
+import "./current-weather.css";
 
 const CurrentWeather = () => {
   return (
     <div className="weather">
-        <img className="weather-icon" src="" alt="weather"></img>
-        <div className="top">
-            <p className="city"></p>
-            <p className="weather-description"></p>
+      <div className="top">
+        <div>
+          <p className="city"> </p>
+          <p className="weather-description"></p>
         </div>
+        <img className="weather-icon" src="icons/01d.png" alt="weather" />
+      </div>
+      <div className="bottom">
+        <p className="temperature">21°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">18°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">18 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humudity</span>
+            <span className="parameter-value">18 %</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">18 C</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default CurrentWeather;
