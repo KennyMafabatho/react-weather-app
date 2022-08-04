bc63bba8a7fde67e81695e00abf9de78eaa4f38a
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2022-08-04 10:42:14 +0200

    Created weather api key and weather url

--- src/api.js
+++ src/api.js
@@ -7,3 +7,6 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/';
+export const WEATHER_API_KEY = '9cdab6a2749accdc20cb626a655913d8';
